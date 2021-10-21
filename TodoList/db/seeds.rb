1bd1ee79e83f99ab7432d8084eccfb503ad71076
pierre <pnavarin@gmail.com>
2021-10-21 15:08:21 -0400

    Finished Phase 1

--- TodoList/db/seeds.rb
+++ TodoList/db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ActiveRecord::Base.transaction do
+  Todolist.destroy_all
+
+  todo1 =  Todolist.create!({
+        title: "wash car",
+        body: "with soap",
+        done: false
+    })
+
+    todo2 = Todolist.create!({
+        title: "wash dog",
+        body: "with shampoo",
+        done: true
+    })
+
+  
+
+end
+
+$.ajax({
+  method: 'GET',
+  url: '/api/todos',
+  data: {
+      todolist: {
+          title: 'test',
+          body: 'test',
+          done: false
+      }
+  },
+  dataType: 'JSON'
+}).then(
+  todos => console.log(todos),
+  error => console.log(error)
+);
